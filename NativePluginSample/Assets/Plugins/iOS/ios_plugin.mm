00e64757904f2ca88f46577a5136f403f81a350f
Takatune <0ta5274738t4p4r@gmail.com>
2020-08-31 23:13:51 +0900

    プラグインにコメント追加

--- NativePluginSample/Assets/Plugins/iOS/ios_plugin.mm
+++ NativePluginSample/Assets/Plugins/iOS/ios_plugin.mm
@@ -3,15 +3,23 @@
 #ifdef __cplusplus
 extern "C"{
 #endif
+
+// プラグインの流れ
+// C# ━ Objective-c ━ swift
+// このソースはC#とswiftを中継する役目
+
+// バックグラウンド再生の設定
 void SetBackgroundMusic(){
     [MusicController4iOS setBackgroundMusic];
 }
 
+// プラグインサンプル（引数あり）
 void SetHello(const char *ch){
     NSString *str = [NSString stringWithCString:ch encoding:NSUTF8StringEncoding];
     [MusicController4iOS setHelloWithStr:str];
 }
 
+// プラグインサンプル（戻り値あり）
 char* GetHello(){
     NSString* str = [MusicController4iOS getHello];
     
@@ -22,11 +30,13 @@ char* GetHello(){
     return retC;
 }
 
+// 引数で受け取ったファイルパスの曲再生
 void PlayMusic(const char *ch){
     NSString *path = [NSString stringWithCString:ch encoding:NSUTF8StringEncoding];
     [MusicController4iOS playMusicWithPath:path];
 }
 
+// 曲停止
 void StopMusic(){
     [MusicController4iOS stopMusic];
 }
--- NativePluginSample/Assets/Plugins/iOS/ios_plugin.swift
+++ NativePluginSample/Assets/Plugins/iOS/ios_plugin.swift
@@ -1,21 +1,30 @@
 import Foundation
 import AVFoundation
 
+// プラグインの流れ
+// C# ━ Objective-c ━ swift
+// このソースはswiftでiOS特有の処理を行う役目
 @objc public class MusicController4iOS:NSObject{
     
+    // プラグインの引数保持と戻り値用
     private static var show:String = "";
+    // ミュージックプレイヤ
     private static var musicPlayer : MusicPlayer?;
+    // シーケンサ
     private static var sequence : MusicSequence?;
+    // セッション
     private static let session = AVAudioSession.sharedInstance();
     
+    // バックグラウンド再生設定
     @objc public static func setBackgroundMusic(){
-        //print("Hello Swift");
+        // カテゴリ設定
         do{
             try session.setCategory(AVAudioSession.Category.playback)
         }catch{
             fatalError("カテゴリ設定失敗");
         }
         
+        // セッション有効化
         do{
             try session.setActive(true);
         }catch{
@@ -23,6 +32,7 @@ import AVFoundation
         }
     }
     
+    // 引数ありプラグインサンプル
     @objc public static func setHello(str:String){
         if(FileManager.default.fileExists(atPath:str)){
             show.append("Exists this file path!!\n");
@@ -31,27 +41,30 @@ import AVFoundation
         show.append("\n");
     }
     
+    // 戻り値ありプラグインサンプル
     @objc public static func getHello() -> String{
         show.append("\nHello Swift");
         return show;
     }
     
+    // パス指定曲再生
     @objc public static func playMusic(path:String){
-        let midiFile = URL(fileURLWithPath: path)//Bundle.main.url(forResource: path, withExtension: "mid");
+        // URLから曲ファイル取得
+        let midiFile = URL(fileURLWithPath: path);
+        // インスタンス取得
         NewMusicPlayer(&musicPlayer);
         NewMusicSequence(&sequence);
         
-        print("File >>");
-        print(midiFile);
-        print(path);
-        
         if let musicPlayer = musicPlayer, let sequence = sequence {
+            // 曲読み込み
             MusicSequenceFileLoad(sequence, midiFile as! CFURL, .midiType, MusicSequenceLoadFlags());
             MusicPlayerSetSequence(musicPlayer, sequence);
+            // 曲再生
             MusicPlayerStart(musicPlayer);
         }
     }
     
+    // 曲停止
     @objc public static func stopMusic(){
         if let musicPlayer = musicPlayer, let sequence = sequence {
             MusicPlayerStop(musicPlayer);
